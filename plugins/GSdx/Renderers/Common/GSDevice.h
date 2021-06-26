27b99d9381a771e9dd90502af619d459d9ec32a5
twinaphex <libretro@gmail.com>
2021-06-26 17:48:56 +0200

    Remove ExternalFX

--- plugins/GSdx/Renderers/Common/GSDevice.h
+++ plugins/GSdx/Renderers/Common/GSDevice.h
@@ -82,16 +82,6 @@ public:
 	InterlaceConstantBuffer() {memset(this, 0, sizeof(*this));}
 };
 
-class ExternalFXConstantBuffer
-{
-public:
-	GSVector2 xyFrame;
-	GSVector4 rcpFrame;
-	GSVector4 rcpFrameOpt;
-
-	ExternalFXConstantBuffer() { memset(this, 0, sizeof(*this)); }
-};
-
 class FXAAConstantBuffer
 {
 public:
@@ -157,7 +147,6 @@ protected:
 	virtual void DoMerge(GSTexture* sTex[3], GSVector4* sRect, GSTexture* dTex, GSVector4* dRect, const GSRegPMODE& PMODE, const GSRegEXTBUF& EXTBUF, const GSVector4& c) = 0;
 	virtual void DoInterlace(GSTexture* sTex, GSTexture* dTex, int shader, bool linear, float yoffset) = 0;
 	virtual void DoFXAA(GSTexture* sTex, GSTexture* dTex) {}
-	virtual void DoExternalFX(GSTexture* sTex, GSTexture* dTex) {}
 	virtual uint16 ConvertBlendEnum(uint16 generic) = 0; // Convert blend factors/ops from the generic enum to DX11/OGl specific.
 
 public:
@@ -215,7 +204,6 @@ public:
 	void Merge(GSTexture* sTex[3], GSVector4* sRect, GSVector4* dRect, const GSVector2i& fs, const GSRegPMODE& PMODE, const GSRegEXTBUF& EXTBUF, const GSVector4& c);
 	void Interlace(const GSVector2i& ds, int field, int mode, float yoffset);
 	void FXAA();
-	void ExternalFX();
 
 	bool ResizeTexture(GSTexture** t, int type, int w, int h);
 	bool ResizeTexture(GSTexture** t, int w, int h);
