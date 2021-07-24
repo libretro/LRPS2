1553eb60a78c2d9c6d449ccfc7620c72b2ec41e7
twinaphex <libretro@gmail.com>
2021-07-24 17:57:45 +0200

    Backport https://github.com/PCSX2/pcsx2/commit/29f83b4364cd0f21631c419823b297e5ffc08c19

--- pcsx2/gui/AppCoreThread.cpp
+++ pcsx2/gui/AppCoreThread.cpp
@@ -25,7 +25,6 @@
 #include "GS.h"
 
 #include "CDVD/CDVD.h"
-#include "SPU2/spu2.h"
 #include "Elfheader.h"
 #include "Patch.h"
 #include "R5900Exceptions.h"
@@ -195,7 +194,6 @@ void AppCoreThread::Resume()
 	}
 #endif
 	GetCorePlugins().Init();
-	SPU2init();
 	_parent::Resume();
 }
 
