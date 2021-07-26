ea96b382d1bb42b75fc041b749235fe8ddfc1e2e
twinaphex <libretro@gmail.com>
2021-07-26 09:35:43 +0200

    retro_core_option_definition - use constants instead of manually typed strings

--- libretro/libretro_core_options.h
+++ libretro/libretro_core_options.h
@@ -4,12 +4,12 @@
 #include <string.h>
 #include <libretro.h>
 #include <retro_inline.h>
+#include "options_enums.h"
 #endif
-
 
 struct retro_core_option_definition option_defs[] = {
 
-	{"pcsx2_bios",
+	{STRING_PCSX2_OPT_BIOS,
 	"System: BIOS",
 	NULL,
 	{
@@ -17,7 +17,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	NULL},
 
-	{"pcsx2_system_language",
+	{STRING_PCSX2_OPT_SYSTEM_LANGUAGE,
 	"System: Language",
 	"Set the BIOS system Language. Useful for PAL multilanguage games. Fastboot option must be disabled. The selected language will be applied, if available in-game. (Content restart required)",
 	{
@@ -32,7 +32,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"English"},
 
-	{"pcsx2_fastcdvd",
+	{BOOL_PCSX2_OPT_FASTCDVD,
 	"System: Fast Loading",
 	"A hack that reduces loading times by setting a faster disc access mode. Check the HDLoader compatibility list for games that will not work with this (Content restart required)",
 	{
@@ -42,7 +42,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled"},
 
-	{"pcsx2_fastboot",
+	{BOOL_PCSX2_OPT_FASTBOOT,
 	"System: Fast Boot",
 	"Bypass the initial BIOS logo. (Content restart required)",
 	{
@@ -52,7 +52,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled"},
 
-	{"pcsx2_boot_bios",
+	{BOOL_PCSX2_OPT_BOOT_TO_BIOS,
 	"System: Boot To BIOS",
 	"Skip the content loading and boot in the BIOS. Useful to manage memory cards currently associated to this content. (Content restart required)",
 	{
@@ -62,7 +62,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled"},
 
-	{"pcsx2_memcard_slot_1",
+	{STRING_PCSX2_OPT_MEMCARD_SLOT_1,
 	"Memory Card: Slot 1",
 	"Select the primary memory card to use. 'Legacy' points to the memory card Mcd001 in the old location system/pcsx2/memcards. (Content restart required)",
 	{
@@ -70,7 +70,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	NULL},
 
-	{ "pcsx2_memcard_slot_2",
+	{STRING_PCSX2_OPT_MEMCARD_SLOT_2,
 	"Memory Card: Slot 2",
 	"Select the secondary memory card to use. 'Legacy' points to the memory card Mcd002 in the old location system/pcsx2/memcards. (Content restart required)",
 	{
@@ -79,7 +79,7 @@ struct retro_core_option_definition option_defs[] = {
 	NULL },
 
 
-	{"pcsx2_renderer",
+	{STRING_PCSX2_OPT_RENDERER,
 	"Video: Renderer",
 	"Content reboot required",
 	{
@@ -92,7 +92,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"Auto"},
 
-	{"pcsx2_upscale_multiplier",
+	{INT_PCSX2_OPT_UPSCALE_MULTIPLIER,
 	"Video: Internal Resolution ",
 	NULL,
 	{
@@ -107,7 +107,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"1"},
 
-	{"pcsx2_deinterlace_mode",
+	{INT_PCSX2_OPT_DEINTERLACING_MODE,
 	"Video: Deinterlacing Mode",
 	"Can remove blur on some games. Note that some modes can slightly decrease performance while the others can blur the whole picture, decreasing the amount of details thus deinterlacing is to be used only when it's necessary.",
 	{
@@ -123,7 +123,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"7"},
 
-	{"pcsx2_aspect_ratio",
+	{INT_PCSX2_OPT_ASPECT_RATIO,
 	"Video: Aspect Ratio",
 	"Sets the aspect ratio. Setting the aspect ratio to Widescreen (16:9) stretches the display. For proper widescreen in select games, also turn on the 'Enable Widescreen Patches' option. (Content restart required)",
 	{
@@ -133,7 +133,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0"},
 
-	{"pcsx2_enable_widescreen_patches",
+	{BOOL_PCSX2_OPT_ENABLE_WIDESCREEN_PATCHES,
 	"Video: Enable Widescreen Patches",
 	"Enables widescreen patches that allow certain games to render in true 16:9 ratio without stretching the display. For the widescreen patches to display properly, the 'Aspect Ratio' option should be set to Widescreen (16:9). (Content restart required)",
 	{
@@ -143,7 +143,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled"},
 
-	{ "pcsx2_fxaa",
+	{INT_PCSX2_OPT_FXAA,
 	"Video: FXAA",
 	"Enables Fast Approximate Anti-Aliasing",
 	{
@@ -153,7 +153,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{"pcsx2_anisotropic_filter",
+	{INT_PCSX2_OPT_ANISOTROPIC_FILTER,
 	 "Video: Anisotropic Filtering",
 	 "Reduces texture aliasing at extreme viewing angles",
 	{
@@ -166,7 +166,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_texture_filtering",
+	{INT_PCSX2_OPT_TEXTURE_FILTERING,
 	"Video: Texture Filtering",
 	"Controls the texture filtering of the emulation "
 		"\nNearest: always disable interpolation, rendering will be blocky. "
@@ -182,7 +182,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"2" },
 
-	{ "pcsx2_mipmapping",
+	{INT_PCSX2_OPT_MIPMAPPING,
 	 "Video: Mipmapping",
 	 "Control the accuracy level of the mipmapping emulation."
 		"\nAutomatic: Automatically sets the mipmapping level based on the game.This is the recommended setting."
@@ -199,7 +199,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"-1" },
 
-	{ "pcsx2_conservative_buffer",
+	{BOOL_PCSX2_OPT_CONSERVATIVE_BUFFER,
 	"Video: Conservative Buffer Allocation",
 	"Disabled: Reserves a larger framebuffer to prevent FMV flickers. Increases GPU/memory requirements."
 		"\n\nDisabling this can amplify stuttering due to low RAM/VRAM."
@@ -212,7 +212,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"enabled" },
 
-	{ "pcsx2_accurate_date",
+	{BOOL_PCSX2_OPT_ACCURATE_DATE,
 	"Video: Accurate DATE",
 	"Implement a more accurate algorithm to compute GS destination alpha testing. It improves shadow and transparency rendering."
 				"\nNote: Direct3D 11 is less accurate.",
@@ -224,7 +224,7 @@ struct retro_core_option_definition option_defs[] = {
 	"enabled" },
 
 
-	{ "pcsx2_frameskip",
+	{BOOL_PCSX2_OPT_FRAMESKIP,
 	"Video: Frame Skip",
 	NULL,
 	{
@@ -234,7 +234,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled" },
 
-	{ "pcsx2_frames_to_draw",
+	{INT_PCSX2_OPT_FRAMES_TO_DRAW,
 	"Video: Frameskip - Frames to Draw",
 	NULL,
 	{
@@ -252,7 +252,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"1" },
 
-	{ "pcsx2_frames_to_skip",
+	{INT_PCSX2_OPT_FRAMES_TO_SKIP,
 	"Video: Frameskip - Frames to Skip",
 	NULL,
 	{
@@ -270,7 +270,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"1" },
 
-	{"pcsx2_rumble_enable",
+	{BOOL_PCSX2_OPT_GAMEPAD_RUMBLE_ENABLE,
 	"Gamepad: Enable Rumble",
 	"Enables rumble on gamepads that support it",
 	{
@@ -280,7 +280,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"enabled"},
 
-	{"pcsx2_rumble_intensity",
+	{INT_PCSX2_OPT_GAMEPAD_RUMBLE_FORCE,
 	"Gamepad: Rumble Intensity",
 	"Intensity of gamepad rumble",
 	{
@@ -298,7 +298,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"100"},
 
-	{ "pcsx2_enable_cheats",
+	{BOOL_PCSX2_OPT_ENABLE_CHEATS,
 	"Patches: Enable Cheats",
 	"Enabled: Checks the 'system/pcsx2/cheats' directory for a PNACH file for the running content and, if found, \
 	applies the cheats from the file. (Content restart required)",
@@ -309,7 +309,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled" },
 
-	{"pcsx2_speedhacks_presets",
+	{INT_PCSX2_OPT_SPEEDHACKS_PRESET,
 	 "Emulation: Speed Hacks Preset",
 	 "Preset which controls the balance between accuracy and speed. (Content restart required) \
 		\nSafest: no speed hacks. Most reliable, but possibly slow. \
@@ -328,7 +328,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"1" },
 
-	{ "pcsx2_vsync_mtgs_queue",
+	{INT_PCSX2_OPT_VSYNC_MTGS_QUEUE,
 	"Emulation: Vsyncs in MTGS Queue",
 	"Setting this to a lower value improves input lag, a value around 2 or 3 will slightly improve framerates.",
 	{
@@ -340,7 +340,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"2" },
 
-	{ "pcsx2_clamping_mode",
+	{INT_PCSX2_OPT_CLAMPING_MODE,
 	"Emulation: Clamping Mode",
 	"Clamping mode can fix some bugs on some games. Default value is fine for most games. (Content restart required)",
 	{
@@ -353,7 +353,7 @@ struct retro_core_option_definition option_defs[] = {
 	"1" },
 
 
-	{ "pcsx2_round_mode",
+	{INT_PCSX2_OPT_ROUND_MODE,
 	"Emulation: Round Mode",
 	"Round mode can fix some bugs on some games. Default value is fine for most games. (Content restart required)",
 	{
@@ -366,7 +366,7 @@ struct retro_core_option_definition option_defs[] = {
 	"3" },
 
 
-	{ "pcsx2_userhack_align_sprite",
+	{BOOL_PCSX2_OPT_USERHACK_ALIGN_SPRITE,
 	"Hack: Align Sprite",
 	"Fixes vertical lines problem in some games when resolution is upscaled.",
 	{
@@ -376,7 +376,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled" },
 
-	{ "pcsx2_userhack_merge_sprite",
+	{BOOL_PCSX2_OPT_USERHACK_MERGE_SPRITE,
 	"Hack: Merge Sprite",
 	"Another option which could fix vertical lines problem in some games when resolution is upscaled.",
 	{
@@ -387,7 +387,7 @@ struct retro_core_option_definition option_defs[] = {
 	"disabled" },
 
 
-	{ "pcsx2_userhack_skipdraw_start",
+	{INT_PCSX2_OPT_USERHACK_SKIPDRAW_START,
 	"Hack: Skipdraw - Start Layer",
 	"Used to fix some rendering glitches and bad post processing by skipping rendering layers.",
 	{
@@ -416,7 +416,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_skipdraw_layers",
+	{INT_PCSX2_OPT_USERHACK_SKIPDRAW_LAYERS,
 	"Hack: Skipdraw - Layers to Skip",
 	"Number of rendering layers to skip, counting from the value set in the Start Layer option. For a original PCSX2 setting of 2:5, skipdraw option of the core must be set 2 +3 .",
 	{
@@ -445,7 +445,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_halfpixel_offset",
+	{INT_PCSX2_OPT_USERHACK_HALFPIXEL_OFFSET,
 	"Hack: Half-pixel Offset",
 	"Might fix some misaligned fog, bloom or blend effect. The preferred option is Normal(vertex).",
 	{
@@ -457,7 +457,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_round_sprite",
+	{INT_PCSX2_OPT_USERHACK_ROUND_SPRITE,
 	"Hack: Round Sprite",
 	"Corrects the sampling of 2D textures when upscaling.",
 	{
@@ -468,7 +468,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_wildarms_offset",
+	{BOOL_PCSX2_OPT_USERHACK_WILDARMS_OFFSET,
 	"Hack: Wild Arms Offset",
 	"Avoid gaps between pixel in some games when upscaling.",
 	{
@@ -478,7 +478,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled" },
 
-	{ "pcsx2_userhack_halfscreen_fix",
+	{INT_PCSX2_OPT_USERHACK_HALFSCREEN_FIX,
 	"Hack: Half-screen fix",
 	"Automatic control of the halfscreen fix detection on texture shuffle. Force-Disable may help in some games, but causes visual glitches in most. Use Force-Enable when a game has half screen issues.",
 	{
@@ -489,7 +489,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"-1" },
 
-	{ "pcsx2_userhack_auto_flush",
+	{BOOL_PCSX2_OPT_USERHACK_AUTO_FLUSH,
 	"Hack: Auto Flush",
 	"Force a primitive flush when a framebuffer is also an input texture. Fixes some processing effects such as the shadows in the \
 	Jak series and radiosity in GTA:SA. Very costly in performance. (Content restart required)",
@@ -500,7 +500,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"disabled" },
 
-	{ "pcsx2_userhack_texture_offset_x_hundreds",
+	{INT_PCSX2_OPT_USERHACK_TEXTURE_OFFSET_X_HUNDREDS,
 	"Hack: Texture Offset X - Hundreds",
 	"Set Texture Offset X (sum of X options Hundreds and Tens). \n Offset for the ST/UV texture coordinates. Fixes some odd texture issues and might fix some post processing alignment too.",
 	{
@@ -518,7 +518,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_texture_offset_x_tens",
+	{INT_PCSX2_OPT_USERHACK_TEXTURE_OFFSET_X_TENS,
 	"Hack: Texture Offset X - Tens",
 	"Set Texture Offset X (sum of X options Hundreds and Tens). \n Offset for the ST/UV texture coordinates. Fixes some odd texture issues and might fix some post processing alignment too.",
 	{
@@ -536,7 +536,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_texture_offset_y_hundreds",
+	{INT_PCSX2_OPT_USERHACK_TEXTURE_OFFSET_Y_HUNDREDS,
 	"Hack: Texture Offset Y - Hundreds",
 	"Set Texture Offset Y (sum of Y options Hundreds and Tens). \n Offset for the ST/UV texture coordinates. Fixes some odd texture issues and might fix some post processing alignment too.",
 	{
@@ -554,7 +554,7 @@ struct retro_core_option_definition option_defs[] = {
 	},
 	"0" },
 
-	{ "pcsx2_userhack_texture_offset_y_tens",
+	{INT_PCSX2_OPT_USERHACK_TEXTURE_OFFSET_Y_TENS,
 	"Hack: Texture Offset Y - Tens",
 	"Set Texture Offset Y (sum of Y options Hundreds and Tens). \n Offset for the ST/UV texture coordinates. Fixes some odd texture issues and might fix some post processing alignment too.",
 	{
