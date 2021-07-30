3b26bc47a19bcf7d3492b1ab370930161065d245
twinaphex <libretro@gmail.com>
2021-07-30 18:17:49 +0200

    Remove recguard.h

--- 3rdparty/wxwidgets3.0/src/msw/basemsw.cpp
+++ 3rdparty/wxwidgets3.0/src/msw/basemsw.cpp
@@ -29,11 +29,6 @@
 
 #include "wx/apptrait.h"
 #include "wx/evtloop.h"
-// MBN: this is a workaround for MSVC 5: if it is not #included in
-// some wxBase file, wxRecursionGuard methods won't be exported from
-// wxBase.dll, and MSVC 5 will give linker errors
-#include "wx/recguard.h"
-
 #include "wx/crt.h"
 #include "wx/msw/private.h"
 
