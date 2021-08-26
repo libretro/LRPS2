addb3d2ea1936285b239c52ab3896d9e6e950203
twinaphex <libretro@gmail.com>
2021-08-26 20:35:53 +0200

    (IPU) Backport https://github.com/PCSX2/pcsx2/commit/7dd4fd16fd203974bab0ee684af33e64d1dfdb5b

--- pcsx2/IPU/IPU.cpp
+++ pcsx2/IPU/IPU.cpp
@@ -569,9 +569,9 @@ static __ri bool ipuPACK(tIPU_CMD_CSC csc)
 
 static void ipuSETTH(u32 val)
 {
-	s_thresh[0] = (val & 0xff);
-	s_thresh[1] = ((val >> 16) & 0xff);
-	IPU_LOG("SETTH (Set threshold value)command %x.", val&0xff00ff);
+	s_thresh[0] = (val & 0x1ff);
+	s_thresh[1] = ((val >> 16) & 0x1ff);
+	IPU_LOG("SETTH (Set threshold value)command %x.", val&0x1ff01ff);
 }
 
 // --------------------------------------------------------------------------------------
