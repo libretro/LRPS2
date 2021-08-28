3bfd0fb2824e7e52beff6e6f7e86107f7df73978
twinaphex <libretro@gmail.com>
2021-08-28 12:54:37 +0200

    Revert "remove real CD/DVD drive functionality for Linux for now - was"
    This reverts commit 8f3cefe18369ee4a681aeb7905730e2f0139abfc.

--- pcsx2/CDVD/Linux/DriveUtility.cpp
+++ pcsx2/CDVD/Linux/DriveUtility.cpp
@@ -16,20 +16,70 @@
 #include "PrecompiledHeader.h"
 #include "../CDVDdiscReader.h"
 
+#ifdef __linux__
+#include <libudev.h>
+#include <linux/cdrom.h>
+#endif
+
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
 std::vector<std::string> GetOpticalDriveList()
 {
+#ifdef __linux__
+	udev* udev_context = udev_new();
+	if (!udev_context)
+		return {};
+
+	std::vector<std::string> drives;
+	udev_enumerate* enumerate = udev_enumerate_new(udev_context);
+	if (enumerate)
+	{
+		udev_enumerate_add_match_subsystem(enumerate, "block");
+		udev_enumerate_add_match_property(enumerate, "ID_CDROM_DVD", "1");
+		udev_enumerate_scan_devices(enumerate);
+		udev_list_entry* devices = udev_enumerate_get_list_entry(enumerate);
+
+		udev_list_entry* dev_list_entry;
+		udev_list_entry_foreach(dev_list_entry, devices)
+		{
+			const char* path = udev_list_entry_get_name(dev_list_entry);
+			udev_device* device = udev_device_new_from_syspath(udev_context, path);
+			const char* devnode = udev_device_get_devnode(device);
+			if (devnode)
+				drives.push_back(devnode);
+			udev_device_unref(device);
+		}
+		udev_enumerate_unref(enumerate);
+	}
+	udev_unref(udev_context);
+
+	return drives;
+#else
 	return {};
+#endif
 }
 
 void GetValidDrive(std::string& drive)
 {
 	if (!drive.empty())
 	{
+#ifdef __linux__
+		int fd = open(drive.c_str(), O_RDONLY | O_NONBLOCK);
+		if (fd != -1)
+		{
+			if (ioctl(fd, CDROM_GET_CAPABILITY, 0) == -1)
+				drive.clear();
+			close(fd);
+		}
+		else
+		{
+			drive.clear();
+		}
+#else
 		drive.clear();
+#endif
 	}
 	if (drive.empty())
 	{
