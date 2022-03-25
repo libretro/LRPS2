7b6395f57028351597105c30954995c223f5552e
twinaphex <libretro@gmail.com>
2022-03-25 22:05:15 +0100

    Backport https://github.com/PCSX2/pcsx2/pull/5027/commits/164203d82101afb419c91ed60df25b50a498e4b3 and https://github.com/PCSX2/pcsx2/pull/5027/commits/360ce45aa312595790a952d78a66e0cefdd29809

--- pcsx2/SPU2/Mixer.cpp
+++ pcsx2/SPU2/Mixer.cpp
@@ -231,7 +231,7 @@ static __forceinline void GetNextDataDummy(V_Core& thiscore, uint voiceidx)
 		vc.SCurrent = 0;
 	}
 
-	vc.SP -= 4096 * (4 - (vc.SCurrent & 3));
+	vc.SP -= 0x1000 * (4 - (vc.SCurrent & 3));
 	vc.SCurrent += 4 - (vc.SCurrent & 3);
 }
 
@@ -397,10 +397,10 @@ static __forceinline s32 GetVoiceValues(V_Core& thiscore, uint voiceidx)
 		}
 		vc.PV2 = vc.PV1;
 		vc.PV1 = GetNextDataBuffered(thiscore, voiceidx);
-		vc.SP -= 4096;
+		vc.SP -= 0x1000;
 	}
 
-	const s32 mu = vc.SP + 4096;
+	const s32 mu = vc.SP + 0x1000;
 
 	switch (InterpType)
 	{
@@ -541,7 +541,7 @@ static __forceinline StereoOut32 MixVoice(uint coreidx, uint voiceidx)
 	}
 	else
 	{
-		while (vc.SP > 0)
+		while (vc.SP >= 0)
 			GetNextDataDummy(thiscore, voiceidx); // Dummy is enough
 
 	}
