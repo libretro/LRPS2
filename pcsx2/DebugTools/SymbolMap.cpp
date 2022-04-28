19fbc89d7a98fefde3fbd2905582884a7053ef2a
twinaphex <libretro@gmail.com>
2022-04-28 04:16:20 +0200

    Don't try to use wxFopen when not needed

--- pcsx2/DebugTools/SymbolMap.cpp
+++ pcsx2/DebugTools/SymbolMap.cpp
@@ -46,7 +46,7 @@ void SymbolMap::Clear() {
 
 bool SymbolMap::LoadNocashSym(const char *filename) {
 	std::lock_guard<std::recursive_mutex> guard(m_lock);
-	FILE *f = wxFopen(filename, "r");
+	FILE *f = fopen(filename, "r");
 	if (!f)
 		return false;
 
