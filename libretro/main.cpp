0b41a485a76d0388f677b1587bacaea30cac639f
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2022-08-11 16:06:39 +0100

    Remove excess folders
    Removed the pcsx2 > bios folder requirement for the core. BIOS files can now be placed in just the system folder.

--- libretro/main.cpp
+++ libretro/main.cpp
@@ -264,7 +264,7 @@ void retro_init(void)
 
 	// get the BIOS available and fill the option
 
-	bios_dir = Path::Combine(system, "pcsx2/bios");
+	bios_dir = Path::Combine(system);
 
 	wxArrayString bios_list;
 	wxDir::GetAllFiles(bios_dir.GetFullPath(), &bios_list, L"*.*", wxDIR_FILES);
@@ -965,5 +965,5 @@ wxString GetExecutablePath()
 {
 	const char* system = nullptr;
 	environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &system);
-	return Path::Combine(system, "pcsx2/PCSX2");
+	return Path::Combine(system);
 }
