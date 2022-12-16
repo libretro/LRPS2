fc7452d3ba135c471f685dc32f02e261159d429d
libretroadmin <reallibretroretroarch@gmail.com>
2022-12-16 14:25:07 +0100

    Cmake cleanups

--- cmake/SelectPcsx2Plugins.cmake
+++ cmake/SelectPcsx2Plugins.cmake
@@ -11,18 +11,6 @@ macro(print_dep str dep)
         message(STATUS "${str}:${dep}")
 endmacro(print_dep)
 
-#-------------------------------------------------------------------------------
-#								Pcsx2 core & common libs
-#-------------------------------------------------------------------------------
-# Check for additional dependencies.
-# If all dependencies are available, including OS, build it
-#-------------------------------------------------------------------------------
-set(GTKn_FOUND FALSE)
-
-#---------------------------------------
-#			Common libs
-# requires: -wx
-#---------------------------------------
 if(wxWidgets_FOUND)
     set(common_libs TRUE)
 elseif(NOT EXISTS "${CMAKE_SOURCE_DIR}/common/src")
@@ -32,46 +20,6 @@ else()
     print_dep("Skip build of common libraries: missing dependencies" "${msg_dep_common_libs}")
 endif()
 
-#---------------------------------------
-#			Pcsx2 core
-# requires: -wx
-#           -zlib
-#           -common_libs
-#           -aio
-#---------------------------------------
-# Common dependancy
-if(wxWidgets_FOUND AND ZLIB_FOUND AND common_libs AND NOT (Linux AND NOT AIO_FOUND))
-    set(pcsx2_core TRUE)
-elseif(NOT EXISTS "${CMAKE_SOURCE_DIR}/pcsx2")
-    set(pcsx2_core FALSE)
-else()
-    set(pcsx2_core FALSE)
-    print_dep("Skip build of pcsx2 core: missing dependencies" "${msg_dep_pcsx2}")
-endif()
-
-
-#-------------------------------------------------------------------------------
-#								Plugins
-#-------------------------------------------------------------------------------
-# Check all plugins for additional dependencies.
-# If all dependencies of a plugin are available, including OS, the plugin will
-# be build.
-#-------------------------------------------------------------------------------
-
-#---------------------------------------
-#			GSdx
-#---------------------------------------
-# requires: -OpenGL
-#           -PNG
-#           -X11
-#           -zlib
-#---------------------------------------
+set(pcsx2_core TRUE)
 set(GSdx TRUE)
-#---------------------------------------
-
-#---------------------------------------
-#			USBnull
-#---------------------------------------
 set(USBnull TRUE)
-#---------------------------------------
-#-------------------------------------------------------------------------------
