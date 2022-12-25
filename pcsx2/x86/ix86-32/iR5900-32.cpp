afef40a7f26871cbf0cd07f08b1ec8ab8fd2ee13
libretroadmin <reallibretroretroarch@gmail.com>
2022-12-25 15:47:09 +0100

    Backport x86/iR5900: Ignore double jr/jalr branches like others

--- pcsx2/x86/ix86-32/iR5900-32.cpp
+++ pcsx2/x86/ix86-32/iR5900-32.cpp
@@ -1481,6 +1481,15 @@ void recompileNextInstruction(int delayslot)
 	if( delayslot ) {
 		bool check_branch_delay = false;
 		switch(_Opcode_) {
+			case 0:
+				switch (_Funct_)
+				{
+					case 8: // jr
+					case 9: // jalr
+						check_branch_delay = true;
+						break;
+				}
+				break;
 			case 1:
 				switch(_Rt_) {
 					case 0: case 1: case 2: case 3: case 0x10: case 0x11: case 0x12: case 0x13:
@@ -1490,6 +1499,7 @@ void recompileNextInstruction(int delayslot)
 
 			case 2: case 3: case 4: case 5: case 6: case 7: case 0x14: case 0x15: case 0x16: case 0x17:
 				check_branch_delay = true;
+				break;
 		}
 		// Check for branch in delay slot, new code by FlatOut.
 		// Gregory tested this in 2017 using the ps2autotests suite and remarked "So far we return 1 (even with this PR), and the HW 2.
