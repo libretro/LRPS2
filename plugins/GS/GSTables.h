8b305c57e1f4d26900487593bd4e4194a0c6baec
libretroadmin <reallibretroretroarch@gmail.com>
2022-12-27 20:51:05 +0100

    (GS) Move CLUT tables to GSClut.cpp and make them static

--- plugins/GS/GSTables.h
+++ plugins/GS/GSTables.h
@@ -35,7 +35,3 @@ extern const u8 columnTable32[8][8];
 extern const u8 columnTable16[8][16];
 extern const u8 columnTable8[16][16];
 extern const u16 columnTable4[16][32];
-extern const u8 clutTableT32I8[128];
-extern const u8 clutTableT32I4[16];
-extern const u8 clutTableT16I8[32];
-extern const u8 clutTableT16I4[16];
